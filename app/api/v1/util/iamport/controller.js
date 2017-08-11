b007c03312c52bfa8eea5ea3cfcfa446d396d66c
yusunglee <lys0333@gmail.com>
2017-08-11 17:05:16 +0900

    아이엠포트 훅 로직 수정

--- app/api/v1/util/iamport/controller.js
+++ app/api/v1/util/iamport/controller.js
@@ -1,3 +1,4 @@
+const model = require('./../../../../model')
 const { Iamporter, IamporterError } = require('iamporter')
 const { utilService, orderService } = require('../../service')
 const returnMsg = require('../return.msg')
@@ -7,32 +8,22 @@ const iamporter = new Iamporter({
 })
 exports.importHook = (req, res) => {
   let body = req.body
-
   iamporter.findByImpUid(req.body.imp_uid)
-  .then(r => {
-    let data = r
-    if (r.status === 200) {
-      return orderService.findOneOrderById(r.data.merchant_uid)
-    } else {
-      returnMsg.error400InvalidCall(res, 'ERROR_INVALID_PARAM', 'ERROR_INVALID_PARAM')
-      throw 'hook error'
-    }
-  })
-  .then(r => {
-    return orderService.updateOrderById(r.order_key, {
-      status: r.data.status,
-      order_pay_price	: r.order_receipt_price
+    .then(r => {
+      let data = r
+      if (r.status === 200) {
+        model.Order.update({ status: r.data.status, order_pay_price: r.data.amount }, { where: { order_id: r.data.merchant_uid }, returning: true })
+          .then(r => {
+            if (r[0][1].status === 'paid') {
+              utilService.sendOrderConfirmSms(r[0][1])
+              utilService.sendOrderConfirmSmsToMaker(r[0][1])
+            } else if (r.status === 'ready') {
+              utilService.sendOrderReadySms(r[0][1])
+            }
+            returnMsg.success200RetObj(res, {success: true})
+          })
+      }
     })
-  })
-  .then(r => {
-    if (r[1][0].status === 'paid') {
-      utilService.sendOrderConfirmSms(r[1][0])
-      utilService.sendOrderConfirmSmsToMaker(r[1][0])
-    } else if (r[1][0].status === 'ready') {
-      utilService.sendOrderReadySms(r[1][0])
-    }
-    returnMsg.success200RetObj(res, {success: true})
-  })
-  .catch(val => { returnMsg.error400InvalidCall(res, 'ERROR_INVALID_PARAM', val) })
+    .catch(val => { returnMsg.error400InvalidCall(res, 'ERROR_INVALID_PARAM', val) })
 }
 
