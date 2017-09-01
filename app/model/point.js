65a77202548c798b762be556c62e2442e3832c94
yusunglee2074 <lys0333@gmail.com>
2017-09-01 14:05:20 +0900

    포인트 모델 수정

--- app/model/point.js
+++ app/model/point.js
@@ -5,12 +5,14 @@ module.exports = function (sequelize, DataTypes) {
     point_key: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     user_key: { type: DataTypes.INTEGER, allowNull: false, references: { model: model.User, key: 'user_key' } },
     point_change: { type: DataTypes.INTEGER, allowNull: false },
-    due_date_be_written_days: { type: DataTypes.INTEGER, allowNull: true },
+    due_date_be_written_days: { type: DataTypes.DATE, allowNull: true },
     point_use_percentage: { type: DataTypes.INTEGER, allowNull: true, defaultValue: 100 },
+    type: { type: DataTypes.INTEGER, allowNull: true, defaultValue: null },
   }, {
     tableName: 'point',
     freezeTableName: true,
     underscored: true,
-    timestamps: true
+    timestamps: true,
+    paranoid: true
   })
 }
