faf8a0d8ccf550dd0026726cd5551cea695b646c
yusunglee2074 <lys0333@gmail.com>
2017-09-12 11:14:27 +0900

    포인트 v2 api 사용하게 함

--- app/index.js
+++ app/index.js
@@ -46,7 +46,7 @@ app.use(`${CURRENT_API_VERSION}/iamport`, require('./api/v1/util/iamport'))
 app.use(`${CURRENT_API_VERSION}/home`, require('./api/v1/home'))
 app.use(`${CURRENT_API_VERSION}/qna`, require('./api/v1/qna'))
 app.use(`${CURRENT_API_VERSION}/admin/front`, require('./api/v1/admin'))
-app.use(`${CURRENT_API_VERSION}/point`, require('./api/v1/point'))
+app.use(`${CURRENT_API_VERSION}/point`, require('./api/v2/point'))
 app.use(
   function errorHandler(err, req, res, next) {
     res.status(501)
