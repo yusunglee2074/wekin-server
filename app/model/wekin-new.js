7b99e94bc35abb04bd4808bb0a4949ddb0dd5c37
yusunglee2074 <lys0333@gmail.com>
2017-09-19 15:50:41 +0900

    새로운 위킨, 엑티비티 모델 작성

---
+++ app/model/wekin-new.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const model = require('./index')
+
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('WekinNew', {
+    wekin_key: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    activity_key: { type: DataTypes.INTEGER, allowNull: false, references: { model: model.ActivityNew, key: 'activity_key' } },
+    final_price: { type: DataTypes.INTEGER, allowNull: false },
+    start_date: { type: DataTypes.DATE, allowNull: false },
+    start_time: { type: DataTypes.DATE, allowNull: false },
+    select_option: { type: DataTypes.DATE, allowNull: false },
+    pay_amount: { type: DataTypes.INTEGER, allowNull: false },
+  }, {
+    classMethods: {},
+    tableName: 'wekin',
+    freezeTableName: true,
+    underscored: true,
+    timestamps: true,
+    paranoid: true
+  })
+}
