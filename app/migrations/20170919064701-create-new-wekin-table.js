21984ec1d4adb529f8f5f03fa9aa9c6d95cc53ed
yusunglee2074 <lys0333@gmail.com>
2017-09-20 13:31:25 +0900

    위킨, 엑티비티 테이블 추가 마이그레이션 작성

--- app/migrations/20170919064701-create-new-wekin-table.js
+++ app/migrations/20170919064701-create-new-wekin-table.js
@@ -8,14 +8,15 @@ module.exports = {
       { 
         wekin_key: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
         activity_key: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'ActivityNew', key: 'activity_key' } },
+        user_key: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'user', key: 'user_key' } },
         final_price: { type: Sequelize.INTEGER, allowNull: false },
         start_date: { type: Sequelize.DATE, allowNull: false },
         start_time: { type: Sequelize.DATE, allowNull: false },
-        select_option: { type: Sequelize.DATE, allowNull: false },
+        select_option: { type: Sequelize.JSON, allowNull: false },
         pay_amount: { type: Sequelize.INTEGER, allowNull: false },
-        created_at: { type: Sequelize.DATE, allowNull: true, defaultValue: Sequelize.NOW },
-        updated_at: { type: Sequelize.DATE, allowNull: true },
-        deleted_at: { type: Sequelize.DATE, allowNull: true },
+        created_at: { type: Sequelize.DATE },
+        updated_at: { type: Sequelize.DATE },
+        deleted_at: { type: Sequelize.DATE },
       })
   },
 
