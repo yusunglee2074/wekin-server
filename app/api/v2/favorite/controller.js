1c2d85117b3a7dc7825828079a1a91107b42ccbd
yusunglee2074 <lys0333@gmail.com>
2017-09-30 17:15:54 +0900

    favorite 가져오기 현재 위킨에 맞게 수정

--- app/api/v2/favorite/controller.js
+++ app/api/v2/favorite/controller.js
@@ -1,24 +1,23 @@
 const model = require('../../../model')
 const returnMsg = require('../../../return.msg')
 
-exports.getData = (req, res) => {
+exports.getData = (req, res, next) => {
   model.Favorite.findAll({
-    group: ['Activity.activity_key', 'Favorite.fav_key', 'Activity->Host.host_key'],
+    group: ['ActivityNew.activity_key', 'Favorite.fav_key', 'ActivityNew->Host.host_key'],
     where: { user_key: req.params.user_key },
     include: [{
-      attributes: [
-          'activity_key', 'status', 'host_key', 'main_image', 'title', 'intro_summary', 'address_detail', 'price', 'created_at','address',
-          [model.Sequelize.fn('AVG', model.Sequelize.col('Activity->Docs.activity_key')), 'rating_avg'],
-          [model.Sequelize.fn('COUNT', model.Sequelize.col('Activity->Docs.activity_key')), 'review_count'],
+      attributes: { 
+        include: [
+          [model.Sequelize.fn('AVG', model.Sequelize.col('ActivityNew->Docs.activity_key')), 'rating_avg'],
+          [model.Sequelize.fn('COUNT', model.Sequelize.col('ActivityNew->Docs.activity_key')), 'review_count'],
           [model.Sequelize.fn('COUNT', model.Sequelize.col('Favorite')), 'total']
-        ],
-      model: model.Activity,
-        include: [{ model: model.Host }, { model: model.Doc, attributes: [], where: { type: 1 }, required: false }] }]
+        ]
+      },
+      model: model.ActivityNew,
+      include: [{ model: model.Host }, { model: model.Doc, attributes: [], where: { type: 1 }, required: false }] }]
   })
   .then(result => returnMsg.success200RetObj(res, result))
-  .catch(val => {
-    returnMsg.error400InvalidCall(res, 'ERROR_INVALID_PARAM', val)
-  })
+  .catch(val => next(val))
 }
 
 
