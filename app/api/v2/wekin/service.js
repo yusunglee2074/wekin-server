8e433e0ea7b48a284e43cd55b32c7867b5643b3f
yusunglee2074 <lys0333@gmail.com>
2017-10-18 10:46:47 +0900

    문자 날릴떄 사용하는 위킨 정보 가져오는 함수 New로 수정

--- app/api/v2/wekin/service.js
+++ app/api/v2/wekin/service.js
@@ -2,9 +2,9 @@ const model = require('../../../model')
 
 exports.getWekinByKey = (wekin_key) => {
   return new Promise((resolve, reject) => {
-    model.Wekin.findOne({
-      where: {wekin_key: wekin_key},
-      include: [{model: model.Activity, include: {model: model.Host}}, { model: model.Order }]
+    model.WekinNew.findOne({
+      where: { wekin_key: wekin_key },
+      include: [{ model: model.ActivityNew, include: { model: model.Host } }, { model: model.Order }]
     })
     .then(resolve).catch(reject)
   })
