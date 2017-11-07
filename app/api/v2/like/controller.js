4cb29b1673b4d53940b0ed25eb7aae83c6965d1b
yusunglee2074 <lys0333@gmail.com>
2017-11-07 18:07:23 +0900

    promise 로직 변경

--- app/api/v2/like/controller.js
+++ app/api/v2/like/controller.js
@@ -11,8 +11,8 @@ exports.getData = (req, res) => {
     where: { user_key: req.params.user_key },
     include: {model: model.Doc}
   })
-  .then(result => returnMsg.success200RetObj(res, result))
-  .catch(val => { returnMsg.error400InvalidCall(res, 'ERROR_INVALID_PARAM', val) })
+    .then(result => returnMsg.success200RetObj(res, result))
+    .catch(val => { returnMsg.error400InvalidCall(res, 'ERROR_INVALID_PARAM', val) })
 }
 
 
@@ -26,34 +26,30 @@ exports.putData = (req, res) => {
       },
       transaction: t
     })
-    .then(v => {
-      if(v) {   // 이미 좋아요한 경우 삭제
-        return model.Like.destroy({
-          where: {
+      .then(v => {
+        if(v) {   // 이미 좋아요한 경우 삭제
+          model.Like.destroy({
+            where: {
+              user_key: req.params.user_key,
+              doc_key: req.params.doc_key
+            },
+            returning: true,
+            transaction: t
+          })
+            .then(result => res.json(message: 'success', data: result))
+            .catch(val => next(val))
+
+        } else {    // 좋아요 추가
+          model.Like.create({
             user_key: req.params.user_key,
             doc_key: req.params.doc_key
-          },
-          returning: true,
-          transaction: t
-        })
-        .then(result => returnMsg.success200RetObj(res, result))
-        .catch(val => { returnMsg.error400InvalidCall(res, 'ERROR_INVALID_PARAM', val) })
-        
-      } else {    // 좋아요 추가
-        notiService.docLike(req.params.user_key, req.params.doc_key)
-        notiService.wekinTagLike(req.params.user_key, req.params.doc_key)
-        return model.Like.create({
-          user_key: req.params.user_key,
-          doc_key: req.params.doc_key
-        }, {
-          transaction: t
-        })
-        .then(r => {
-          
-        })
-        .then(result => returnMsg.success200RetObj(res, result))
-        .catch(val => { returnMsg.error400InvalidCall(res, 'ERROR_INVALID_PARAM', val) })
-      }
-    })
+          }, {
+            transaction: t
+          })
+            .then(result => res.json(message: 'success', data: result))
+            .catch(val => next(val) )
+        }
+      })
+      .catch(error => next(error))
   })
 }
