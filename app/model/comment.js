e44cc697d7d25ebb2710719ed8590b4b7cd56842
yusunglee2074 <lys0333@gmail.com>
2017-11-19 21:27:12 +0900

    옛날 모델 FEED 참조하고있었던것 삭제

--- app/model/comment.js
+++ app/model/comment.js
@@ -4,7 +4,7 @@ module.exports = function (sequelize, DataTypes) {
   return sequelize.define('Comment', {
     comment_key: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     user_key: { type: DataTypes.INTEGER, allowNull: false, references: { model: model.User, key: 'user_key' } },
-    doc_key: { type: DataTypes.INTEGER, allowNull: false, references: { model: model.Feed, key: 'doc_key' } },
+    doc_key: { type: DataTypes.INTEGER, allowNull: false, references: { model: model.Doc, key: 'doc_key' } },
     content: { type: DataTypes.TEXT, allowNull: false },
     user_name: { type: DataTypes.STRING(32), allowNull: true },
     user_profile_image: { type: DataTypes.STRING(512), allowNull: true }
