1e76a2109f7a4f8c47ea2a7cb5900a032d0e41ff
yusunglee2074 <lys0333@gmail.com>
2017-12-28 16:02:17 +0900

    iamport HOOK 들어올때 confirm order해버림

--- app/api/v2/util/iamport/controller.js
+++ app/api/v2/util/iamport/controller.js
@@ -11,8 +11,38 @@ exports.importHook = (req, res, next) => {
   iamporter.findByImpUid(req.body.imp_uid)
     .then(r => {
       let data = r
+      let dataForVerify = r.data
+      let orderModel = {
+        imp_uid: dataForVerify.imp_uid,
+        order_pay_method: dataForVerify.pay_method,
+        order_total_price: dataForVerify.amount,
+        user_email: dataForVerify.buyer_email,
+        user_name: dataForVerify.buyer_name,
+        user_phone: dataForVerify.buyer_tel,
+        order_pay_pg: dataForVerify.pg_provider,
+        receipt_url: dataForVerify.receipt_url,
+        pg_tid: dataForVerify.pg_tid,
+        status: dataForVerify.status,
+        order_at: dataForVerify.paid_at*1000 || new Date(),
+        order_extra: {
+          vbank_date: dataForVerify.vbank_date*1000,
+          vbank_holder: dataForVerify.vbank_holder,
+          vbank_name: dataForVerify.vbank_name,
+          vbank_num: dataForVerify.vbank_num,
+          user_agent: dataForVerify.user_agent
+        }
+      }
+      if (dataForVerify.pay_method !== 'vbank') {
+        orderModel.order_pay_price = dataForVerify.amount
+      } else {
+        orderModel.order_pay_price = 0
+      }
+      model.Order.update(orderModel, {
+        where: { order_id: dataForVerify.merchant_uid },
+        returning: true
+      })
       if (r.status === 200) {
-        model.Order.update({ status: r.data.status, order_pay_price: r.data.amount }, { where: { order_id: r.data.merchant_uid }, returning: true })
+        model.Order.update({ status: r.data.status, order_pay_price: r.data.status === 'paid' ? r.data.amount : 0 }, { where: { order_id: r.data.merchant_uid }, returning: true })
           .then(r => {
             if (r[1][0].status === 'paid') {
               model.WekinNew.update({ state: 'paid' }, { where: { wekin_key: r[1][0].wekin_key } })
